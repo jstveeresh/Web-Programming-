39ffd70a7feef4c5b328266a14be4bc5a1d77b45
unknown <gandikov1@.active.newpaltz.edu>
2014-03-03 08:23:21 -0500

    user name changed

--- Final/inc/functions.php
+++ Final/inc/functions.php
@@ -4,7 +4,7 @@ include_once __DIR__ . '/password.php';
 function GetConnection(){
 global $password;
 
-$conn = mysqli_connect('localhost', 'plotkinm', $password, 'plotkinm_db');
+$conn = mysqli_connect('localhost', 'gandikov1', $password, 'gandikov1_db');
 return $conn;
 }
 
